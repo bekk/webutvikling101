89c34abc96c2fbd72f5e443c5e35a7debf1669a6
Stian Veum Møllersen <stian.moellersen@gmail.com>
2016-02-29 20:36:51 +0100

    Working flickr search

--- del2/assignment/server/server.js
+++ del2/assignment/server/server.js
@@ -2,21 +2,58 @@ var fs = require('fs');
 var http = require('http');
 var path = require('path');
 var url = require('url');
+var qs = require('querystring');
 
 var hyperstream = require('hyperstream');
+var Flickr = require('node-flickr');
+
+var flickr = new Flickr({'api_key': 'ea97a6690f90a628b60e2fd79012c74c'});
 
 var clientPath = path.join(__dirname, '..', 'client');
 
 function insertResult(result) {
   return hyperstream({
-    body: result.map(function(r) {
-      return `<img src="${r.url}" alt="${r.title}" />`
-    }).join('')
+    body: result
+      .map(function(r) {
+        return `<img src="${r.url}" alt="${r.title}" />`
+      })
+      .join('')
+  });
+}
+
+function transformResult(result) {
+  return result.photos.photo.map(function(r) {
+    return {
+      url: `https://farm${r.farm}.staticflickr.com/${r.server}/${r.id}_${r.secret}.jpg`,
+      title: r.title
+    };
+  });
+}
+
+function handleError(err, response) {
+  console.error(err);
+
+  response.statusCode = 500;
+  response.write(err);
+  response.end();
+}
+
+function handleSok(request, response, handler) {
+  var data = '';
+
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+
+  request.on('end', function() {
+    data = qs.parse(data);
+
+    flickr.get('photos.search', {'tags': data.sok, 'per_page': 15}, handler);
   });
 }
 
 var result = [
-  {title: "someTitle", url: "someUrl"}
+  {title: 'someTitle', url: 'someUrl'}
 ];
 
 function handleRequest(request, response) {
@@ -26,15 +63,27 @@ function handleRequest(request, response) {
 
   } else if(/^\/sok.html$/.test(request.url) && request.method === 'POST') {
 
-    fs.createReadStream(path.join(clientPath, 'sok.html'))
-      .pipe(insertResult(result))
+    handleSok(request, response, function(err, result) {
+      if(err) {
+        handleError(err, response);
+      }
+
+      fs.createReadStream(path.join(clientPath, 'sok.html'))
+      .pipe(insertResult(transformResult(result)))
       .pipe(response);
+    });
 
   } else if(/^\/sok$/ && request.method === 'POST') {
 
-    response.setHeader('Content-Type', 'application/json');
-    response.write(JSON.stringify(result));
-    response.end();
+    handleSok(request, response, function(err, result) {
+      if(err) {
+        handleError(err, response)
+      }
+
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify(transformResult(result)));
+      response.end();
+    });
 
   } else {
 
