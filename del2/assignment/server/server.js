8daba1e5953dbb4e88e8644383f543dd22cdc737
Kent Andersen <kentareandersen@gmail.com>
2016-03-02 00:06:55 +0100

    fjernet server side resultat, endret søk-endepunktet til GET, restrukturerte serverkode

--- del2/assignment/server/server.js
+++ del2/assignment/server/server.js
@@ -4,21 +4,27 @@ var path = require('path');
 var url = require('url');
 var qs = require('querystring');
 
-var hyperstream = require('hyperstream');
 var Flickr = require('node-flickr');
 
 var flickr = new Flickr({'api_key': 'ea97a6690f90a628b60e2fd79012c74c'});
 
-var clientPath = path.join(__dirname, '..', 'client');
+var siteRoot = path.join(__dirname, '..', 'client');
+var flickerCache = {};
 
-function insertResult(result) {
-  return hyperstream({
-    body: result
-      .map(function(r) {
-        return `<img src="${r.url}" alt="${r.title}" />`
-      })
-      .join('')
+function fetchFlickerContent(tag) {
+  return new Promise(function(resolve, reject) {
+    flickr.get('photos.search', {'tags': tag, 'per_page': 15}, function(err, result) {
+      err ? reject(err) : resolve(result);
+    });
   });
+}
+
+function getFlickerContent(tag) {
+  if(flickerCache[tag]) {
+    return Promise.resolve(flickerCache[tag]);
+  } else {
+    return fetchFlickerContent(tag).then(content => flickerCache[tag] = content);
+  }
 }
 
 function transformResult(result) {
@@ -38,67 +44,35 @@ function handleError(err, response) {
   response.end();
 }
 
-function handleSok(request, response, handler) {
-  var data = '';
-
-  request.on('data', function(chunk) {
-    data += chunk;
-  });
-
-  request.on('end', function() {
-    data = qs.parse(data);
-
-    flickr.get('photos.search', {'tags': data.sok, 'per_page': 15}, handler);
-  });
-}
-
-var result = [
-  {title: 'someTitle', url: 'someUrl'}
-];
-
 function handleRequest(request, response) {
-  if(/^\/$/.test(request.url)) {
 
-    fs.createReadStream(path.join(clientPath, 'index.html')).pipe(response);
+  if(/^\/sok\?/.test(request.url)) {
+    var tag = url.parse(request.url, true).query.tag;
 
-  } else if(/^\/sok.html$/.test(request.url) && request.method === 'POST') {
-
-    handleSok(request, response, function(err, result) {
-      if(err) {
-        handleError(err, response);
-      }
-
-      fs.createReadStream(path.join(clientPath, 'sok.html'))
-      .pipe(insertResult(transformResult(result)))
-      .pipe(response);
-    });
-
-  } else if(/^\/sok$/ && request.method === 'POST') {
-
-    handleSok(request, response, function(err, result) {
-      if(err) {
-        handleError(err, response)
-      }
-
-      response.setHeader('Content-Type', 'application/json');
-      response.write(JSON.stringify(transformResult(result)));
-      response.end();
-    });
+    getFlickerContent(tag)
+      .then(transformResult)
+      .then(function(result) {
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify(result));
+        response.end();
+      })
+      .catch(err => handleError(err, response));
 
   } else {
 
-    var file = path.join(clientPath, request.url);
-    try{
-      fs.statSync(file);
-      fs.createReadStream(file).pipe(response);
-    } catch(e) {
-      response.statusCode = 404;
-      response.end();
-    }
+    var filePath = path.join(siteRoot, request.url);
+    var fallbackPath = path.join(siteRoot, 'index.html');
 
+    fs.stat(filePath, (err, stat) => {
+      if(err || !stat.isFile()) {
+        filePath = fallbackPath;
+      }
+      fs.createReadStream(filePath).pipe(response);
+    });
   }
 }
 
 var server = http.createServer(handleRequest);
 
 server.listen(5000);
+console.log('Server started on http://localhost:5000');
